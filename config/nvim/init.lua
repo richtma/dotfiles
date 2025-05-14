e105f82a02f05c304b27b0890ca8cf7eceeb3701
richtma <richtma@gmail.com>
2025-05-14 19:21:49 +0000

    typo cleaned

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,6 +1,6 @@
-#
-# Shamelessly stolen from various places out there in the wild.
-#
+--
+-- Shamelessly stolen from various places out there in the wild.
+--
 require('config.options')
 require('config.keybinds')
 require('config.lazy')
