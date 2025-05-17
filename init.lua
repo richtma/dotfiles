5f75d2de6f8499cf43bfe4c92278bb7db4018791
richtma <richtma@gmail.com>
2025-05-17 18:54:26 +0000

    markdown preview removed

--- init.lua
+++ init.lua
@@ -97,11 +97,6 @@ require("lazy").setup({
     end
   },
 
--- Markdown preview
-  { "iamcco/markdown-preview.nvim", build = "cd app && npm install", ft = { "markdown" }, init = function()
-      vim.g.mkdp_auto_start = 1
-    end
-  },
 })
 
 -------------------
